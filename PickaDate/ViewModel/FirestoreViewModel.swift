4e19631a5d251aab07f9abd118fe460fa056d25b
eminseo2003 <eminseo2003@naver.com>
2025-02-22 19:35:14 +0900

    firebase 연동 함수 생성

--- PickaDate/ViewModel/FirestoreViewModel.swift
+++ PickaDate/ViewModel/FirestoreViewModel.swift
@@ -11,6 +11,7 @@ import FirebaseFirestore
 class FirestoreViewModel: ObservableObject {
     private let fsDB = Firestore.firestore()
     @Published var userTestData: [UserTest] = []
+    @Published var personalSchedule: [PersonalSchedule] = []
     
     //테스트용
     func fetchUsers() {
@@ -54,6 +55,52 @@ class FirestoreViewModel: ObservableObject {
             } else {
                 print("[L]삭제 성공")
                 self.fetchUsers()
+            }
+        }
+    }
+    func fetchPersonalSchedules() {
+        fsDB.collection("personalSchedule").getDocuments { snapshot, error in
+            if let error = error {
+                print("[E]데이터 가져오기 실패: \(error.localizedDescription)")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                self.personalSchedule = snapshot?.documents.compactMap { doc in
+                    try? doc.data(as: PersonalSchedule.self)
+                } ?? []
+            }
+        }
+    }
+    
+    func addPersonalSchedule(userID: String, name: String, content: String, groupID: [String], schedule: [TimeSlotPersonal]) {
+        
+        let personalSchedule: [String: Any] = [
+            "userID": userID,
+            "name": name,
+            "content": content,
+            "createdAt": FieldValue.serverTimestamp(),
+            "schedule": schedule,
+            "groupID": groupID
+        ]
+        
+        fsDB.collection("userTestData").document(userID).setData(personalSchedule) { error in
+            if let error = error {
+                print("[E]추가 실패: \(error.localizedDescription)")
+            } else {
+                print("[L]문서 추가 성공")
+                self.fetchPersonalSchedules()
+            }
+        }
+    }
+    
+    func deletePersonalSchedule(userId: String) {
+        fsDB.collection("personalSchedule").document(userId).delete { error in
+            if let error = error {
+                print("[E]삭제 실패: \(error.localizedDescription)")
+            } else {
+                print("[L]삭제 성공")
+                self.fetchPersonalSchedules()
             }
         }
     }
