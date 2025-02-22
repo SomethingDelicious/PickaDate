fad0dad47a22a76f0d9d310515ee3b287d910660
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-02-22 17:43:54 +0900

    [0.0.1] 그룹 일정 뷰: 기본 틀

--- PickaDate/View/GroupDateView.swift
+++ PickaDate/View/GroupDateView.swift
@@ -8,11 +8,29 @@
 import SwiftUI
 
 struct GroupDateView: View {
+    // MARK: - Properties
+    
+    // MARK: - Body
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                // TODO: 캘린더 섹션 구현하기
+                Text("캘린더 영역")
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 300)
+                    .background(Color.gray.opacity(0.1))
+                
+                // TODO: 일정 목록 섹션 구현하기
+                List {
+                    Text("일정 목록 영역")
+                }
+            }
+            .navigationTitle("그룹 일정")
+        }
     }
 }
 
+// MARK: - Preview
 #Preview {
     GroupDateView()
 }
