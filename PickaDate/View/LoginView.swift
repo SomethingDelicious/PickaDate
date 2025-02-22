24839f288e154da0e8c95037b099402da77571da
Yeonah Shim <simya0528@naver.com>
2025-02-22 18:22:06 +0900

    LoginView 추가

---
+++ PickaDate/View/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  PickaDate
+//
+//  Created by 심연아 on 2/22/25.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
