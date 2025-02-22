1e115b84f6fe0728369260e9e5f28255a1a14700
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-02-22 20:59:26 +0900

    [0.0.2] 그룹 일정 뷰: 캘린더 UI 추가

--- PickaDate/View/GroupDateView.swift
+++ PickaDate/View/GroupDateView.swift
@@ -9,24 +9,43 @@ import SwiftUI
 
 struct GroupDateView: View {
     // MARK: - Properties
+    @State private var selectedDate = Date() // 선택된 날짜 저장
     
     // MARK: - Body
     var body: some View {
         NavigationStack {
             VStack {
-                // TODO: 캘린더 섹션 구현하기
-                Text("캘린더 영역")
-                    .frame(maxWidth: .infinity)
-                    .frame(height: 300)
-                    .background(Color.gray.opacity(0.1))
+                // 캘린더 섹션
+                DatePicker(
+                    "날짜 선택",
+                    selection: $selectedDate,
+                    displayedComponents: [.date]
+                )
+                .datePickerStyle(.graphical) // 달력 스타일
+                .padding()
                 
-                // TODO: 일정 목록 섹션 구현하기
+                // 선택된 날짜 표시
+                Text("선택된 날짜: \(formattedDate(selectedDate))")
+                    .font(.headline)
+                
+                // 일정 목록 섹션 (더미 데이터로 우선 구현하기)
                 List {
-                    Text("일정 목록 영역")
+                    ForEach(1...3, id: \.self) { index in
+                        Text("예시 일정 \(index)")
+                    }
                 }
             }
             .navigationTitle("그룹 일정")
         }
+    }
+    
+    // MARK: - Methods
+    // Date 타입을 받아서 형식화된 문자열로 변환하는 함수
+    private func formattedDate(_ date: Date) -> String {
+        let formatter = DateFormatter() // 날짜 형식을 지정하는 포메터 생성
+        formatter.dateFormat = "yyyy년 MM월 dd일" // 날짜 형식을 지정
+        formatter.locale = Locale(identifier: "ko_KR") // 한국어 로케일 설정
+        return formatter.string(from: date) // 날짜를 문자열로 변환하여 번환
     }
 }
 
