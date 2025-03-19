1d836ad7d0e89bc5e5a42f881201698d13a763c2
Jiyong-ko <kjy90516@naver.comkjy90516@daum.net>
2025-03-20 02:13:25 +0900

    [0.7.3] 셋팅에 로그아웃 기능 추가

--- PickaDate/View/SettingView.swift
+++ PickaDate/View/SettingView.swift
@@ -8,11 +8,24 @@
 import SwiftUI
 
 struct SettingView: View {
+    @EnvironmentObject private var authService: AuthService
+    
     var body: some View {
-        Text("SettingView")
+        NavigationView {
+            List {
+                // 로그아웃 버튼을 위한 섹션
+                Section {
+                    // 로그아웃 버튼 (아직 기능 미구현)
+                    Button("로그아웃") {
+                        authService.signOut()
+                    }
+                    .foregroundColor(.red) // 경고성 액션이므로 빨간색으로 표시
+                } header: {
+                    Text("계정")
+                }
+            }
+            .navigationTitle("설정")
+        }
     }
 }
 
-#Preview {
-    SettingView()
-}
